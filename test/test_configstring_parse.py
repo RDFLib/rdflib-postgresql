ef7fdd8f7cb1410a9d04370161dd3cd5710b9e4b
Graham Higgins <gjh-github@bel-epa.com>
2012-03-23 18:20:26 +0000

    Add configstring parse test, disable performance test by default.

---
+++ test/test_configstring_parse.py
@@ -0,0 +1,35 @@
+from nose import SkipTest
+import unittest
+from rdflib_postgresql.PostgreSQL import ParseConfigurationString
+from rdflib_postgresql.PostgreSQL import GetConfigurationString
+
+class PostgreSQLStoreConfigTests(unittest.TestCase):
+    storetest = True
+    store_name = "PostgreSQL"
+    create = True
+    configString="user=dog59,password=asecret,host=localhost,db=test"
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_PostgreSQL_getconfig(self):
+        dsn = "port=5432 host=localhost password=asecret user=dog59 dbname=test"
+        res = GetConfigurationString(self.configString)
+        assert res == dsn, repr(res)
+
+    def test_PostgreSQL_parseconfig(self):
+        kvDict = dict(user="dog59",password="asecret",host="localhost",port=5432, db="test")
+        res = ParseConfigurationString(self.configString)
+        assert res == kvDict, repr(res)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# To enable profiling data, use nose's built-in hookup with hotshot:
+# nosetests --with-profile --profile-stats-file stats.pf test/test_postgresql
+# Also see Tarek Ziade's gprof2dot explorations:
+# http://tarekziade.wordpress.com/2008/08/25/visual-profiling-with-nose-and-gprof2dot/
